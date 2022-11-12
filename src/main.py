9c35331e2d7724b148489f5c96d0ec1a5eb3f6a6
Sivert <sivelud@gmail.com>
2022-11-12 18:44:24 +0100

    fiksa men fortsatt rar bug

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 from tracker import MovementDetector
 import cv2
 
-movement_detector = MovementDetector()
+cap = cv2.VideoCapture(0)
 
-while True:
+movement_detector = MovementDetector(cap)
+
+running = False
+
+if cap.isOpened():
+    running = True
+else:
+    print("ERROR: Camera failed to open")
+
+while running:
     
     movement_detector.update()
+    movement_detector.showMask()
+    #print(movement_detector.PercentageOfMovement(100, 500, 100, 500))
 
     key = cv2.waitKey(30)
     if key == 27:
         break
 
-movement_detector.destroy()
+
+cap.release()
+cv2.destroyAllWindows()
--- src/tracker.py
+++ src/tracker.py
@@ -2,18 +2,32 @@ import cv2
 
 
 class MovementDetector():
-    def __init__(self):
-        self.cap = cv2.VideoCapture(0)
+    def __init__(self, cap):
+        self.cap = cap
         self.object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
     def update(self):
-        ret, frame = self.cap.read()
-        height, width, _ = frame.shape
-        roi = frame[340: 720,500: 800]
+        ret, self.frame = self.cap.read()
+        height, width, _ = self.frame.shape
+        
+
+    def showMask(self):
+        roi = self.frame[340: 720,500: 800]
         mask = self.object_detector.apply(roi)
         cv2.imshow("Mask", mask)
 
-    
-    def destroy(self):
-        self.cap.release()
-        cv2.destroyAllWindows()
+    def PercentageOfMovement(self, x1, x2, y1, y2):
+        roi = self.frame[y1: y2,x1: x2]
+        mask = self.object_detector.apply(roi)
+        
+        w = 0
+        b = 0
+        for i in mask:
+            for j in i:
+                if j:
+                    w+=1
+                    b+=1
+                else:
+                    b+=1
+        return(100*(w/(b*100)))
+        
