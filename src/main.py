04969ccb8d4ae9c99d54680768d2972a0bb1f875
Sivert <sivelud@gmail.com>
2022-11-12 19:19:17 +0100

    små forbedringer

--- src/main.py
+++ src/main.py
@@ -12,20 +12,23 @@ running = False
 if cap.isOpened():
     running = True
 else:
-    print("ERROR: Camera failed to open")
+    print("ERROR: Camera failed to open. This is probably because the program was not exited correctly")
 
 while running:
     
     movement_detector.update()
+
+    # Nessecary in order for the program to exit correctly. Will probably be obsolete later in the project.
     movement_detector.showMask()
 
-    # Kills camera. needs fix
     print(movement_detector.PercentageOfMovement())
+
+    movement_detector.updateRoi(0, 1000, 0, 1000)
 
     key = cv2.waitKey(30)
     if key == 27:
         break
 
-
+# Neccesary for the program to exit correctly. if not computer will need reboot for the camera to work again.
 cap.release()
 cv2.destroyAllWindows()
--- src/tracker.py
+++ src/tracker.py
@@ -5,28 +5,35 @@ class MovementDetector():
     def __init__(self, cap):
         self.cap = cap
         self.object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+        ret, self.frame = self.cap.read()
+        height, width, _ = self.frame.shape
+        self.roi = self.frame[340: 720,500: 800]
 
     def update(self):
         ret, self.frame = self.cap.read()
         height, width, _ = self.frame.shape
-        
+        self.mask = self.object_detector.apply(self.roi)
 
     def showMask(self):
-        self.roi = self.frame[340: 720,500: 800]
-        self.mask = self.object_detector.apply(self.roi)
         cv2.imshow("Mask", self.mask)
 
     def PercentageOfMovement(self):
-        w = 0
-        b = 0
+        w = 0 # White pixels
+        t = 0 # Total number of pixels
+        n = 0
+
+        # Goes over all pixels in mask
         for i in self.mask:
             for j in i:
                 if j:
                     w+=1
-                    b+=1
+                    t+=1
                 else:
-                    b+=1
-        return(100*(w/(b*100)))
+                    t+=1
+        # Percentage of pixels that are white
+        if w and t:
+            n = int(w/t*100)
+        return(n)
 
     def updateRoi(self, x1, x2, y1, y2):
         self.roi = self.frame[y1: y2,x1: x2]
